444ddefe5a60eb39c39339cac22b7bf9284333a2
EslamKamel89 <eslam_kamel_89@hotmail.com>
2025-07-08 09:48:29 +0300

    Add initial implementation of Lock Screen Widget and associated assets

---
+++ ios/LockScreenWidget/LockScreenWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LockScreenWidgetBundle.swift
+//  LockScreenWidget
+//
+//  Created by eslam kamel on 07/07/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LockScreenWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LockScreenWidget()
+    }
+}
